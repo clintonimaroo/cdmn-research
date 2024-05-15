9a8e64f041dda3c87b6164b17a3b0ade013e419a
Clinton Imaro <93707197+clintonimaroo@users.noreply.github.com>
2024-05-15 21:08:41 +0200

    update

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
+import './assets/styles.css';
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false;
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app');
